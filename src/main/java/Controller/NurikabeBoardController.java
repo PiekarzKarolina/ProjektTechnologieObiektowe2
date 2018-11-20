a25000ec37e801ee1f2d3d5d8185e323eac394cd
Karolina <piekarz.karolina@gmail.com>
2018-11-20 22:42:57 +0100

    Decomposing Controllers

--- src/main/java/Controller/NurikabeBoardController.java
+++ src/main/java/Controller/NurikabeBoardController.java
@@ -5,16 +5,13 @@ import Model.Board;
 import Model.Score;
 import javafx.scene.control.Button;
 
-public class NurikabeViewController {
+public class NurikabeBoardController {
     private NurikabeController controller;
     private CommandRegistry commandRegistry;
     private Board board;
     private Score score;
 
-    private Button undoButton;
-    private Button redoButton;
     private Button checkButton;
-    private Button resetButton;
     private Button newGameButton;
     private Button rankButton;
 
@@ -22,22 +19,19 @@ public class NurikabeViewController {
     public void initialize(){
         //TODO
     }
-    public void handleUndoAction(){
-        //TODO
-    }
-    public void handleRedoAction(){
-        //TODO
-    }
     public void handleCheckAction(){
-        //TODO
-    }
-    public void handleResetAction(){
         //TODO
     }
     public void handleColor1(){
         //TODO
     }
     public void handleColor2(){
+        //TODO
+    }
+    public void handleNewGameAction(){
+        //TODO
+    }
+    public void handleRankAction(){
         //TODO
     }
 }
