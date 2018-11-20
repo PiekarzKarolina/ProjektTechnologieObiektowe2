2a70081e89fc942edea28f7906b5a7dc5f5da702
Jan Malina <jmalinaa@gmail.com>
2018-11-20 20:50:12 +0100

    Nurikabe.java edited online with Bitbucket

--- src/main/java/Game/Nurikabe.java
+++ src/main/java/Game/Nurikabe.java
@@ -1,16 +1,26 @@
 package Game;
 
+import Controller.NurikabeController;
+import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Nurikabe {
     private Stage primaryStage;
+    private NurikabeController controller;
 
+    @Override
+    public void start(Stage primaryStage) {
+
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Nurikabe");
+
+        this.controller = new NurikabeController(primaryStage);
+        this.controller.initLayout();
+
+    }
     public static void main(String [] args){
         System.out.println("Game.Nurikabe");
-        //TODO
+        launch(args);
     }
 
-    private void start(Stage stage){
-        //TODO
-    }
 }
